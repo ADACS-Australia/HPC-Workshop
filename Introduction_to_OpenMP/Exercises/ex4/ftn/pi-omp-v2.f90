9f85bfb19d53412043ff35ca22eb813c1aea5456
Mohsin Ahmed Shaikh <mohsinshaikh786@gmail.com>
2017-11-27 19:45:47 +0800

    ex3 and ex4 directories updated with relevant files

---
+++ Introduction_to_OpenMP/Exercises/ex4/ftn/pi-omp-v2.f90
@@ -0,0 +1,25 @@
+! Calculating Pi with using method: 
+!     integral 4.0/(1+x^2) dx = p	     
+! 	   Serial Version	     
+program pi_serial
+implicit none
+integer(kind=8) :: num_steps=10000000000
+integer(kind=8) :: i
+real*8 :: PI_ref=3.1415926535897932,step,x,pi
+real*8 :: summation=0.0
+real   :: start,finish
+call cpu_time(start)
+step = 1/(1.0 * num_steps)
+
+	do i =1,num_steps
+	x=((i-1)+0.5) * step
+summation=summation + (4.0/(1.0+(x*x)))
+	end do
+	pi = step * summation
+call cpu_time(finish)
+	write(*,*) "Estimated value of pi =",pi
+	write(*,*) "Error =", abs(PI_ref - pi)
+	write(*,*) "Compute time=", (finish-start)," seconds"
+	end
+
+
